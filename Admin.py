2b93ec4c4ce2f9258eef397be47766972f68bcbf
chandra-rmdn <afdhyllhanadha@gmail.com>
2025-01-21 23:13:55 +0700

    Initial commit

--- Admin.py
+++ Admin.py
@@ -1,55 +1,100 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages
-from mysql import connector
+import mysql.connector
+from flask import Flask, render_template, request, redirect, url_for
+from mysql.connector import Error
 
 app = Flask(__name__)
 
-db = connector.connect(
-    host ="localhost",
-    user = "root",
-    password = "",
-    database = "db_barang"
-)
+def get_connection():
+    try: 
+        connection = mysql.connector.connect(
+            host = "tlw44.h.filess.io",
+            database = "DataProduk_mineralsis",
+            port = "3307",
+            user = "DataProduk_mineralsis",
+            password = "66d5729d92ed1cad20b08d72d53e271810fc25b2"
+        )
+        return connection
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+        return None
 
-if db.is_connected():
-    print("MySQL Connected")
+try:
+    connection = get_connection()
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+
+def generate_kode_barang():
+    connection = get_connection()
+    if connection:
+        cursor = connection.cursor()
+        try:
+            cursor.execute("SELECT kode_barang FROM data_barang ORDER BY kode_barang DESC LIMIT 1")
+            result = cursor.fetchone()
+
+            if result is None:  # Jika tidak ada data, mulai dari B-001
+                return "B-001"
+            else:
+                last_kode = result[0]  # Ambil kode barang terakhir, misalnya "B-001"
+                number = int(last_kode.split('-')[1]) + 1  # Pisahkan angka, tambah 1
+                return f"B-{number:03d}"
+        except Error as e:
+            print("Error while deleting data:", e)
+        finally:
+            cursor.close()
+            connection.close()
 
 @app.route('/')
 def index():
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM data_barang")
-    result = cursor.fetchall()
-    cursor.close()
-    return render_template('index.html', hasil = result)
+    connection = get_connection()
+    if connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM data_barang")
+        result = cursor.fetchall()
+        cursor.close()
+        connection.close()
+    else:
+        result = []
+        print("Unable to connect to database")
+    return render_template('index.html', hasil=result)
 
 def total_qty():
-    cursor = db.cursor()
-    cursor.execute("SELECT SUM(qty) AS total_qty FROM data_barang")
-    result = cursor.fetchone()
-    total_qty = result[0] if result[0] is not None else 0
-    cursor.close()
+    connection = get_connection()
+    if connection:
+        cursor = connection.cursor()
+        try:
+            cursor.execute("SELECT SUM(qty) AS total_qty FROM data_barang")
+            result = cursor.fetchone()
+            total_qty = result[0] if result[0] is not None else 0
+        except Error as e:
+            print("Error while deleting data:", e)
+        finally:
+            cursor.close()
+            connection.close()
     return total_qty
 
 @app.route('/data_stok/')
 def data_stok():
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM data_barang")
-    result = cursor.fetchall()
-    total_qty_value = total_qty()
-    cursor.close()
+    connection = get_connection()
+    if connection:
+        cursor = connection.cursor()
+        try:
+            cursor.execute("SELECT * FROM data_barang")
+            result = cursor.fetchall()
+            total_qty_value = total_qty()
+        except Error as e:
+            print("Error while deleting data:", e)
+        finally:
+            cursor.close()
+            connection.close()
     return render_template('form_data_stok.html', hasil = result, total_qty = total_qty_value)
-
-def generate_kode_barang():
-    cursor = db.cursor()
-    cursor.execute("SELECT kode_barang FROM data_barang ORDER BY kode_barang DESC LIMIT 1")
-    result = cursor.fetchone()
-    cursor.close()
-
-    if result is None:  # Jika tidak ada data, mulai dari B-001
-        return "B-001"
-    else:
-        last_kode = result[0]  # Ambil kode barang terakhir, misalnya "B-001"
-        number = int(last_kode.split('-')[1]) + 1  # Pisahkan angka, tambah 1
-        return f"B-{number:03d}"
     
 ## Tambah ##    
 @app.route('/tambah/')
@@ -66,18 +111,33 @@ def proses_tambah():
     harga_beli = request.form['harga_beli']
     harga_jual = request.form['harga_jual']
     qty = request.form['qty']
-    cursor = db.cursor()
-    cursor.execute("INSERT INTO data_barang (kode_barang, nama_barang, type, unit, harga_beli, harga_jual, qty) VALUES (%s, %s, %s, %s, %s, %s, %s)", (kode_barang, nama_barang, type, unit, harga_beli, harga_jual, qty))
-    db.commit()
+    connection = get_connection()
+    if connection:
+        cursor = connection.cursor()
+        try:
+            cursor.execute("INSERT INTO data_barang (kode_barang, nama_barang, type, unit, harga_beli, harga_jual, qty) VALUES (%s, %s, %s, %s, %s, %s, %s)", (kode_barang, nama_barang, type, unit, harga_beli, harga_jual, qty))
+            connection.commit()
+        except Error as e:
+            print("Error while inserting data:", e)
+        finally:
+            cursor.close()
+            connection.close()
     return redirect(url_for('index'))
 
 ## Ubah ##
 @app.route('/ubah/<kode_barang>', methods=['GET'])
 def ubah_data(kode_barang):
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM data_barang WHERE kode_barang = %s", (kode_barang,))
-    result = cursor.fetchall()
-    cursor.close()
+    connection = get_connection()
+    if connection:
+        cursor = connection.cursor()
+        try:
+            cursor.execute("SELECT * FROM data_barang WHERE kode_barang = %s", (kode_barang,))
+            result = cursor.fetchall()
+        except Error as e:
+            print("Error while inserting data:", e)
+        finally:
+            cursor.close()
+            connection.close()
     return render_template('form_ubah.html', hasil = result)
 
 ## Ubah Form Data Barang ##
@@ -91,18 +151,34 @@ def proses_ubah():
     harga_beli = request.form['harga_beli']
     harga_jual = request.form['harga_jual']
     qty = request.form['qty']
-    cursor = db.cursor()
-    sql = 'UPDATE data_barang SET kode_barang=%s, nama_barang=%s, type=%s, unit=%s, harga_beli=%s, harga_jual=%s, qty=%s WHERE kode_barang=%s'
-    value = (kode_barang, nama_barang, type, unit, harga_beli, harga_jual, qty, kode)
-    cursor.execute(sql, value)
-    db.commit()
+    connection = get_connection()
+    if connection:
+        cursor = connection.cursor()
+        try:
+            sql = 'UPDATE data_barang SET kode_barang=%s, nama_barang=%s, type=%s, unit=%s, harga_beli=%s, harga_jual=%s, qty=%s WHERE kode_barang=%s'
+            value = (kode_barang, nama_barang, type, unit, harga_beli, harga_jual, qty, kode)
+            cursor.execute(sql, value)
+            connection.commit()
+        except Error as e:
+            print("Error while inserting data:", e)
+        finally:
+            cursor.close()
+            connection.close()
     return redirect(url_for('index'))
 
 @app.route('/hapus/<kode_barang>', methods=['GET'])
 def hapus_data(kode_barang):
-    cursor = db.cursor()
-    cursor.execute('DELETE from data_barang WHERE kode_barang=%s', (kode_barang,))
-    db.commit()
+    connection = get_connection()
+    if connection:
+        cursor = connection.cursor()
+        try:
+            cursor.execute('DELETE from data_barang WHERE kode_barang=%s', (kode_barang,))
+            connection.commit()
+        except Error as e:
+            print("Error while inserting data:", e)
+        finally:
+            cursor.close()
+            connection.close()
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
